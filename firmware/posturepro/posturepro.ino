dd61af2f49ed04ce99a529c1ee9edf41d4f09e77
Maryam Ali <maryam1@seas.upenn.edu>
2025-01-20 19:41:20 -0500

    added preliminary calibration methods

--- firmware/posturepro/posturepro.ino
+++ firmware/posturepro/posturepro.ino
@@ -2,6 +2,7 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include "limits.h"
 // #include <ArduinoBLE.h>
 
 #define PACKET_SIZE 9
@@ -14,28 +15,62 @@ MyoWare emg;
 Adafruit_MPU6050 mpu;
 int status;
 float packet[PACKET_SIZE];
-float calibrated_packet[PACKET_SIZE];
+float rest[PACKET_SIZE];
+float max_emg;
 
-void reset(packet) {
+void reset(float* packet) {
   for (int i = 0; i < PACKET_SIZE; ++i) {
     packet[i] = 0;
   }
 }
 
-void calibrate() {
-  // Initialize calibration packet
-  reset(calibrated_packet);
+void calibrate_rest() {
+  // Initialize rest array
+  Serial.println("Starting baseline calibration...");
+  reset(rest);
 
-  // Calibration routine
-  for (int i = 0; i < 500; ++i) {
+  unsigned long startTime = millis();
+  int numValues = 0;
+
+  while (millis() - startTime < 10000) {
     read_imu(&mpu, packet);
-    calibrated_packet[0] += packet[0];
-    calibrated_packet[1] += packet[1];
-    calibrated_packet[2] += packet[2];
-    calibrated_packet[3] += packet[3];
-    calibrated_packet[4] += packet[4];
-    calibrated_packet[5] += packet[5];
+    read_emg(&emg, packet);
+    for (int j = 0; j < PACKET_SIZE; ++j) {
+      rest[j] += packet[j];
+    }
+    numValues += 1;
   }
+
+  for (int j = 0; j < PACKET_SIZE; ++j) {
+      rest[j] = rest[j] / numValues;
+  }
+
+  // Serial.print("The rest value for AccX is: ");
+  // Serial.println(rest[0]);
+  // Serial.print("The rest value for AccY is: ");
+  // Serial.println(rest[1]);
+  // Serial.print("The rest value for AccZ is: ");
+  // Serial.println(rest[2]);
+
+  Serial.println("Completed baseline calibration.");
+}
+
+void calibrate_emg() {
+  Serial.println("Calibrating EMG...");
+  max_emg = INT_MIN;
+
+  Serial.println("Flex your muscle to the maximum extent...");
+  unsigned long startTime = millis();
+  int numValues = 0;
+
+  while (millis() - startTime < 10000) {
+    read_emg(&emg, packet);
+    if (packet[8] > max_emg) {
+      max_emg = packet[8];
+    }
+  }
+
+  Serial.println("Completed EMG calibration.");
 }
 
 void setup() {
@@ -56,9 +91,12 @@ void setup() {
 
   status = initialize_imu(&mpu);
   if (status) {
-    Serial.println("Error initialziing MPU");
+    Serial.println("Error initializing MPU");
     while(1);
   }
+
+  calibrate_rest();
+  calibrate_emg();
 
 }
 
@@ -67,29 +105,29 @@ void loop() {
   read_imu(&mpu, packet);
   read_emg(&emg, packet);
 
-  Serial.println("Sending complete data packet...");
-  int status = send_ble(packet, PACKET_SIZE, true);
+  // Serial.println("Sending complete data packet...");
+  // int status = send_ble(packet, PACKET_SIZE, true);
 
   // Serial.print("EMG: ");
-  // Serial.print(packet[0]);
-  // Serial.print(", ");
-  // Serial.print(packet[1]);
-  // Serial.print(", ");
-  // Serial.println(packet[2]);
-
-  // Serial.print("Acceleration: ");
-  // Serial.print(packet[3]);
-  // Serial.print(", ");
-  // Serial.print(packet[4]);
-  // Serial.print(", ");
-  // Serial.println(packet[5]);
-  
-  // Serial.print("Rotation: ");
   // Serial.print(packet[6]);
   // Serial.print(", ");
   // Serial.print(packet[7]);
   // Serial.print(", ");
   // Serial.println(packet[8]);
+
+  Serial.print("Acceleration: ");
+  Serial.print(packet[0]);
+  Serial.print(", ");
+  Serial.print(packet[1]);
+  Serial.print(", ");
+  Serial.println(packet[2]);
+  
+  Serial.print("Rotation: ");
+  Serial.print(packet[3]);
+  Serial.print(", ");
+  Serial.print(packet[4]);
+  Serial.print(", ");
+  Serial.println(packet[5]);
 
   delay(1000);
 }
